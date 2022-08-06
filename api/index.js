32421415d78a987da766f94acb2154c5105a90ba
Lei Li <li.lei1@northeastern.edu>
2022-08-05 17:29:54 -0700

    ll

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 import cookieParser from "cookie-parser";
+import cors from 'cors';
+
 const app = express()
 dotenv.config()
 
@@ -24,6 +26,7 @@ const connect = async () => {
   });
 
   //middlewares
+  app.use(cors())
   app.use(cookieParser())
   app.use(express.json())
 
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,6 +1,8 @@
 import express from "express";
 import {
 
+  countByCity,
+    countByType,
     createHotel,
     deleteHotel,
     getHotel,
@@ -23,10 +25,13 @@ router.put("/:id",verifyAdmin, updateHotel);
 router.delete("/:id",verifyAdmin, deleteHotel);
 
 //GET
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 
 
 //GET ALL
-router.get("/", getHotels);
+router.get("/", getHotels); 
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", countByType);
 
 export default router
