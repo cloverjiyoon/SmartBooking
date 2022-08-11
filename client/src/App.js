ad6fe69b4d3c0d6d528543b8c80fa8e13afbdc37
Lei Li <li.lei1@northeastern.edu>
2022-08-11 00:56:59 -0700

    leili

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Home from "./pages/home/Home.jsx";
 import Hotel from "./pages/hotel/Hotel.jsx";
 import List from "./pages/list/List.jsx";
 import Login from "./pages/login/login.jsx";
+import Register from "./pages/register/Register.jsx";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/hotels" element={<List/>}/>
         <Route path="/hotels/:id" element={<Hotel/>}/>
         <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
     </BrowserRouter>
   );
